ee1c5be9c247aa3aa2ca0f9ed0d38f116b4694ee
root <m395400082@163.com>
2022-04-11 19:21:42 +0800

    push

--- src/main/java/mm/git/GitTest.java
+++ src/main/java/mm/git/GitTest.java
@@ -8,6 +8,7 @@ public class GitTest {
         System.out.println("Hello git4!");
         System.out.println("master test");
         System.out.println("hot-fix git!");
+        System.out.println("push test!");
 
     }
 }
